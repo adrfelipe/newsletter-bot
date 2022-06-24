fa6854480fad8c0292090ba211a5d6cd52ee2eb5
André Felipe <adrfelipe.sz@gmail.com>
2022-06-24 14:55:36 -0300

    making the stylized embed

--- commands/news.js
+++ commands/news.js
@@ -1,7 +1,7 @@
 const { Command, CommandType } = require('gcommands');
+const { MessageEmbed } = require('discord.js');
 const axios = require('axios')
-
-
+const wait = require('node:timers/promises').setTimeout;
 
 new Command({
 	name: 'news',
@@ -14,10 +14,17 @@ new Command({
         let url = 'https://newsletter-deschamps.herokuapp.com/news'
 
         const { data } = await axios.get(url)
-        const news = data.data
-        
-        console.log(news)
 
-		return ctx.reply(`${data.data[1].titleText}`);
+        const embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${data.data[2].titleText}`)
+            .setAuthor({ name: 'Newsletter do Filipe Deschamps!', iconURL: 'https://filipedeschamps.com.br/avatar-big.png', url: 'https://filipedeschamps.com.br/' })
+            .setDescription(`${data.data[2].mainText}`)
+            .setTimestamp()
+	        .setFooter({ text: 'Open Source Projects', iconURL: 'https://cdn.discordapp.com/icons/951257053194764309/ef582a24d7ae88de45d9a567ad93fadc.png?size=2048' });
+
+        await ctx.deferReply()
+        await wait(4000)
+		return ctx.editReply({embeds: [embed]});
 	}
 });
